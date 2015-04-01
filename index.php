3cb3233c938afd969d119f4422a96ce507f5539c
Joshua Zenn <wordman05@gmail.com>
2015-04-01 10:37:18 -0400

    Create index.php
    Created base PHP/HTML file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<header>
+<title>SampleSite - InterDJ</h1>
+<!-- Add Google Analytics code here -->
+</header>
+<body>
+<h1>SampleSite - InterDJ</h1>
+<h3>List of current stations</h3>
+<p>Click join to start listening!</p>
+</body>
+<footer>
+<hr>
+<p>Created with <a href="https://github.com/ARMmaster17/InterDJ">InterDJ</a></p>
+</footer>
+</html>
