d81db80c3a0d6c14f5ea6d8f6c7c7bf068e83f7c
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-04-01 21:41:00 +0000

    Added Now Playing status

--- listen.php
+++ listen.php
@@ -3,7 +3,24 @@
         <?php require_once 'settings.php' ?>
     </header>
     <body>
-        
+        <?php
+        $db = new mysqli($G_DB_server, $G_DB_uname, $G_DB_passwd, $G_DB_db, $G_DB_serverport);
+        if ($db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }
+        $result = mysqli_query($db, "SELECT * FROM npdb WHERE id = " . $_GET['st']) or die("Query fail: " . mysqli_error());
+
+        //loop the result set
+        $row = mysqli_fetch_array($result);
+        ?>
+        <h1>Station <?php echo $row['sname'] ?></h1>
+        <?php
+        $sid = explode(",", $row['sgarray'])[0];
+        $result2 = mysqli_query($db, "SELECT * FROM sdb WHERE id = " . $sid) or die("Query fail: " . mysqli_error());
+        $row2 = mysqli_fetch_array($result2);
+        ?>
+        <h4>Now Playing: <?php echo $row2['title'] ?> by <?php echo $row2['artist'] ?></h4>
+        <p>Up next: </p>
     </body>
     <footer>
         
