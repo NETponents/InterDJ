043e34aabdedce2373d733cb26ebb3e3830ed122
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-04-02 15:59:48 +0000

    (BAD) Replaced WMP with Plyr

--- listen.php
+++ listen.php
@@ -21,6 +21,17 @@
         $row2 = mysqli_fetch_array($result2);
         ?>
         <h4>Now Playing: <?php echo $row2['title'] ?> by <?php echo $row2['artist'] ?></h4>
+        <script src="dist/plyr.js"></script>
+        <script>
+            document.querySelectorAll(".player")[0].plyr.source("<?php echo $G_URL . "/audio/" . $row2['audiopath'] ?>");
+            document.querySelectorAll(".player")[0].plyr.play();
+            var media = document.querySelectorAll(".player")[0].plyr.media;
+            media.addEventListener("ended", function()
+            { 
+                console.log("Stopped");
+                location.reload();
+            });
+        </script>
         <p>Up next: </p>
         <?php
         for($i = 1; $sid[$i] != null; $i++)
@@ -30,15 +41,6 @@
             echo "<li>" . $row3['title'] . " by " . $row3['artist'] . "</li>";
         }
         ?>
-        <OBJECT ID="MediaPlayer1" CLASSID="CLSID:22d6f312-b0f6-11d0-94ab-0080c74c7e95" CODEBASE="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab# Version=5,1,52,701" STANDBY="Loading Microsoft Windows® Media Player components..." TYPE="application/x-oleobject" width="280" height="46">
- <param name="fileName" value="<?php echo $G_URL . "/audio/" . $row2['audiopath'] ?>">
- <param name="animationatStart" value="true">
- <param name="transparentatStart" value="true">
- <param name="autoStart" value="true">
- <param name="showControls" value="true">
- <param name="Volume" value="-300">
- <embed type="application/x-mplayer2" pluginspage="http://www.microsoft.com/Windows/MediaPlayer/" src="<?php echo $G_URL . "/audio/" . $row2['audiopath'] ?>" name="MediaPlayer1" width=280 height=46 autostart=1 showcontrols=1 volume=-300>
- </OBJECT> 
     </body>
     <footer>
         <?php require_once 'footer.php' ?>
