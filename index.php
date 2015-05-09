b833008308e0adaebdebe9f980cbf74c43e1f2a9
Joshua Zenn <wordman05@gmail.com>
2015-05-08 20:18:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
     <header>
       <?php require_once 'header.php' ?>
-      <title>Bootstrap Example</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
