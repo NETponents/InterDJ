b5e1f4693c7290efa561257492af84258a8c8b49
Joshua Zenn <wordman05@gmail.com>
2015-05-08 20:07:13 -0400

    Update bodyhead.php

--- bodyhead.php
+++ bodyhead.php
@@ -1,4 +1,6 @@
 <?php require_once 'settings.php' ?>
-<h1><?php echo $G_name ?> - InterDJ</h1>
-<h4><?php echo $G_tag ?></h4>
-<hr>
+
+<div class="jumbotron">
+    <h1><?php echo $G_name ?> - InterDJ</h1>
+    <p><?php echo $G_tag ?></p> 
+</div>
